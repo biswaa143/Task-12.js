06f0c7d9cc10b6cfc1e1b29afed0fd89f1de6550
biswa143 <barikbiswa143@gmail.com>
2022-12-13 19:32:10 +0530

    task-12

---
+++ fatarrow.js
@@ -0,0 +1,32 @@
+var getA=function(a){
+    return a;
+};//normal function
+var getA=a=>a;
+console.log(getA(1)); //output:1 fat arrow function
+
+square=a=>a*a;
+console.log(square(2));//output 4
+
+square=(a)=>{return a*a}
+console.log(square(4));//16 or
+
+var a=5;
+square=()=>{return a*a};
+console.log(square());
+
+let mult=(a,b)=>{return a*b};
+console.log(mult(2,4));//output 8
+
+var x=function(){
+    this.val=1;
+    setTimeout(()=>{
+        this.val++;
+        console.log(this.val);
+    },1)
+}
+var y=new x(); //output:2
+
+var x=(...n)=>{
+    console.log(n[0]);
+};
+x(1,2,3); //output:1
